eeb0fbb33c5684ae88b43a4992795c16ba07a4d3
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-13 16:04:56 -0600

    added test for name & id params)

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,10 +1,24 @@
-// import employee class
+// Import employee class
 const Employee = require('../lib/employee');
 
 // Test Employee class
 describe("TEST FOR EMPLOYEE CLASS", () => {
-    it("Should create an employee object", () => {
+
+    it("Should instantiate and create an employee object", () => {
         const emp = new Employee();
         expect(typeof emp).toBe("object")       
+    });
+
+    it("Should have a name property when instantiated with a name parameter", () => {
+        const name = "Missy";
+        const emp = new Employee(name);
+        expect(emp.name).toEqual(name);
+    })
+
+    it("Should have an id property when instantiated with an id parameter", () => {
+        const name = "Missy";
+        const id = 24;
+        const emp = new Employee(name, id);
+        expect(emp.id).toEqual(id);
     })
 })
