f68b5562a6965e8efa8b755c9a86ba49132e9175
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-14 10:19:19 -0600

    cleaned up code

--- index.js
+++ index.js
@@ -133,36 +133,36 @@ function addEmployee() {
 // Function to generate new HTML file with team's contact info
 function renderHTML() {
     const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <!-- Bootstrap link -->
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
-        />
-        <!-- My CSS link -->
-        <link rel="stylesheet" href="../assets/css/style.css" />
-        <title>Team Contacts</title>
-      </head>
-      <body>
-        <header>
-          <h1 class="py-3 py-md-5 display-3 d-flex justify-content-center">
-            My Team
-          </h1>
-        </header>
-        <main class="container">
-          <div class="card-container row d-flex justify-content-center">
-            ${employees.map(employee => employee.renderEmpCard(employee.officeNumber || employee.github || employee.school)).join("\n")}
-          </div>
-        </main>
-      </body>
-    </html>
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <!-- Bootstrap link -->
+            <link
+              href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+              rel="stylesheet"
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous"
+            />
+            <!-- My CSS link -->
+            <link rel="stylesheet" href="./assets/css/style.css" />
+            <title>Team Contacts</title>
+          </head>
+          <body>
+            <header>
+              <h1 class="py-3 py-md-4 display-3 d-flex justify-content-center mb-5 pb-3">
+                My Team
+              </h1>
+            </header>
+            <main class="container">
+              <div class="card-container row d-flex justify-content-center">
+                ${employees.map(employee => employee.renderEmpCard(employee.officeNumber || employee.github || employee.school)).join("\n")}
+              </div>
+            </main>
+          </body>
+        </html>
     `;
 
     fs.writeFileSync("./dist/index.html", html);
